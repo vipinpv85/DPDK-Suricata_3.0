dfa65e4070ad0fd10574b865e210ec56ef92f0e7
Megha Ajmera <megha.ajmera@intel.com>
2019-11-21 14:07:00 +0530

    src: rework on rule add stats
    Signed-off-by: Vipin Varghese <vipinpv85@gmail.com>

--- suricata-3.0/src/detect-engine-analyzer.c
+++ suricata-3.0/src/detect-engine-analyzer.c
@@ -506,82 +506,56 @@ void dpdkRuleAnalysis(Signature *s)
 {
     stats_matchPattern.totalRules++;
     uint8_t checkProto;
-    if (s->alproto == ALPROTO_HTTP)
-    {
-        stats_matchPattern.http ++;
-    }
-    else if (s->alproto == ALPROTO_FTP)
-    {
-        stats_matchPattern.ftp ++;
-    }
-    else if (s->alproto == ALPROTO_SMTP)
-    {
-        stats_matchPattern.smtp ++;
-    }
-    else if (s->alproto == ALPROTO_TLS)
-    {
-        stats_matchPattern.tls ++;
-    }
-    else if (s->alproto == ALPROTO_DNS)
-    {
-        stats_matchPattern.dns ++;
-    }
-    else if (s->alproto == ALPROTO_SSH)
-    {
-        stats_matchPattern.ssh ++;
-    }
-    else if (s->alproto == ALPROTO_SMB)
-    {
-        stats_matchPattern.smb ++;
-    }
-    else if (s->alproto == ALPROTO_SMB2)
-    {
-        stats_matchPattern.smb2 ++;
-    }
-    else if (s->alproto == ALPROTO_DCERPC)
-    {
-        stats_matchPattern.dcerpc ++;
+
+    if (s->alproto) {
+        if (s->alproto == ALPROTO_HTTP)
+            stats_matchPattern.http ++;
+        else if (s->alproto == ALPROTO_FTP)
+            stats_matchPattern.ftp ++;
+        else if (s->alproto == ALPROTO_SMTP)
+            stats_matchPattern.smtp ++;
+        else if (s->alproto == ALPROTO_TLS)
+            stats_matchPattern.tls ++;
+        else if (s->alproto == ALPROTO_DNS)
+            stats_matchPattern.dns ++;
+        else if (s->alproto == ALPROTO_SSH)
+            stats_matchPattern.ssh ++;
+        else if (s->alproto == ALPROTO_SMB)
+            stats_matchPattern.smb ++;
+        else if (s->alproto == ALPROTO_SMB2)
+            stats_matchPattern.smb2 ++;
+        else if (s->alproto == ALPROTO_DCERPC)
+            stats_matchPattern.dcerpc ++;
     }
 
     checkProto = DetectProtoContainsProto(&s->proto, IPPROTO_TCP);
     if(checkProto == 1)
-    {
         stats_matchPattern.tcp ++;
-    }
+
     checkProto = DetectProtoContainsProto(&s->proto, IPPROTO_UDP);
     if(checkProto == 1)
-    {
         stats_matchPattern.udp ++;
-    }
+
     checkProto = DetectProtoContainsProto(&s->proto, IPPROTO_SCTP);
     if(checkProto == 1)
-    {
         stats_matchPattern.sctp ++;
-    }
+
     checkProto = DetectProtoContainsProto(&s->proto, IPPROTO_ICMPV6);
     if(checkProto == 1)
-    {
         stats_matchPattern.icmpv6 ++;
-    }
+
     checkProto = DetectProtoContainsProto(&s->proto, IPPROTO_GRE);
     if(checkProto == 1)
-    {
         stats_matchPattern.gre ++;
-    }
+
     checkProto = DetectProtoContainsProto(&s->proto, IPPROTO_RAW);
     if(checkProto == 1)
-    {
         stats_matchPattern.raw ++;
-    }
-    if (s->proto.flags & DETECT_PROTO_IPV4)
-    {
-        stats_matchPattern.ipv4 ++;
-    }
-    if (s->proto.flags & DETECT_PROTO_IPV6)
-    {
-        stats_matchPattern.ipv6 ++;
-    }
 
+    if (s->proto.flags & DETECT_PROTO_IPV4)
+        stats_matchPattern.ipv4 ++;
+    if (s->proto.flags & DETECT_PROTO_IPV6)
+        stats_matchPattern.ipv6 ++;
 }
 
 #endif 
